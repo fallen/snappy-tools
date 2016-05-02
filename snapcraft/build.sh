fad61e2001163630168dad0d7d50b6cdd5a7a56d
Patrick Boettcher <patrick.boettcher@posteo.de>
2016-05-02 17:15:45 +0200

    libshdata: build.sh update for rpi

--- snapcraft/build.sh
+++ snapcraft/build.sh
@@ -3,10 +3,14 @@ export TARGET_ARCH='arm'
 export TARGET_LIBC='eglibc'
 export TARGET_OS='parrot'
 #export TARGET_CPU='tegrax1' # Tegra X1
-export TARGET_CPU='p7'    # Raspi
+export TARGET_CPU='armv7a'    # Raspi
 export USE_CONFIG_CHECK=1
 export NO_CLONE=1
-export ALCHEMY_TARGET_SCAN_ADD_DIRS=~/parrot/packages/
+export ALCHEMY_TARGET_SCAN_ADD_DIRS=$HOME/parrot/packages/
+export ALCHEMY_PATH=$HOME/parrot/build/alchemy 
 #export TARGET_CROSS=/opt/arm-2014.11-linaro/bin/arm-linux-gnueabihf-
+export TARGET_FLOAT_ABI=hard
+export TARGET_TEST=1
 
-snapcraft snap
+
+~/.local/bin/snapcraft snap
